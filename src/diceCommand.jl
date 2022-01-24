c6e498b7fcf9888c3e9ff8755d0f461ebb3672e3
phyxmeow <XueHaotian@outlook.com>
2022-01-24 19:48:29 +0800

    Fix \(Really\) .bot exit.

--- src/diceCommand.jl
+++ src/diceCommand.jl
@@ -260,7 +260,7 @@ function botSwitch(argstr; groupId = "")
         groupConfigs[groupId] = cp
         return DiceReply("悟理球不知道哪里去了~")
         @case "exit"
-        sendMessage("悟理球从这里消失了", chat_id = parse(Int, groupId))
+        sendMessage(text = "悟理球从这里消失了", chat_id = parse(Int, groupId))
         leaveChat(chat_id = parse(Int, groupId))
         delete!(groupConfigs, groupId)
         return noReply
